d7793803b6b7502d7a8131e9a7cc16fd35511135
Frédéric Lachapelle <38048855+Fredolx@users.noreply.github.com>
2022-04-13 14:47:21 -0400

    Add additionalMimeTypes to HostedContent (#23)
    Co-authored-by: BELL\frederic.lachapelle <frederic.lachapelle@bell.ca>

---
+++ src/SharpWebview/StartupParameters.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpWebview
+{
+    public class StartupParameters
+    {
+        public IDictionary<string, string>? AdditionalMimeTypes { get; set; }
+    }
+}
